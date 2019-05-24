0d638b8ca4b75b7946c2c70b7f3154aca46f6242
jonas.ehrhardt <jonas.ehrhardt@student.htw-berlin.de>
2019-05-24 16:03:04 +0200

    Add SeasonsEvent

--- Assets/Scripts/SeasonsManager.cs
+++ Assets/Scripts/SeasonsManager.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using VRTK;
 
-public class SeasonsManager : MonoBehaviour
+[System.Serializable]
+public class SeasonEvent : UnityEvent<Season>
 {
-    
+}
+
+public class SeasonsManager : Singleton<SeasonsManager>
+{
+    public UnityEvent<Season> UpdateSeason;
+    private Season currentSeason;
+
+    private void Awake()
+    {
+        UpdateSeason = new SeasonEvent();
+    }
+
+    private void Start()
+    {
+        UpdateSeason.Invoke(Season.WINTER);
+    }
 }
