861ba0afc516cf8218add67d94328ba7e8fa19aa
Josef <josef.heitzler@gmx.de>
2019-06-16 20:18:18 +0200

    add TreeSeasonBehaviour

--- Assets/Scripts/TreeSeasonBehaviour.cs
+++ Assets/Scripts/TreeSeasonBehaviour.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class TreeSeasonBehaviour : SeasonObjectBehaviour
 {
+    [SerializeField]
+    private Texture[] textures = null;
+
     public override void UpdateRepresentation(Season currentSeason)
     {
-        Debug.Log("Tree");
+        Debug.Log(currentSeason);
+        foreach (var rend in GetComponentsInChildren<Renderer>())
+        {
+            Debug.Log(rend.name);
+            rend.material.SetTexture("_MainTexture", textures[(int)currentSeason]);
+        }
     }
 }
