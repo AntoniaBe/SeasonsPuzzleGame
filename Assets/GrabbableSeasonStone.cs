27dc5c19813e909ed9fee14a6028d871c23fc2d2
vncnzd <vincenz.dziedzina@gmail.com>
2019-06-17 11:50:19 +0200

    Add script with event firing if season stone is touched

---
+++ Assets/GrabbableSeasonStone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GrabbableSeasonStone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<VRTK_InteractableObject>() == null)
+        {
+            Debug.Log("Season Stone has no InteractableObject script property");
+        } else
+        {
+            GetComponent<VRTK_InteractableObject>().InteractableObjectTouched += SeasonStoneTouched;
+        }
+    }
+
+    private void SeasonStoneTouched(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log(sender);
+        Debug.Log("touched");
+    }
+}
