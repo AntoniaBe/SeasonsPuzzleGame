1cc20dba7298cf841ceea54afaa9f8a624605d25
Josef <josef.heitzler@gmx.de>
2019-06-21 15:14:48 +0200

    add objects with snow animation

---
+++ Assets/Scripts/LerpScaleBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpScaleBehaviour : LerpProperty
+{
+    [SerializeField]
+    private float minFactor;
+
+    private Vector3 min;
+    private Vector3 max;
+    private bool direction = true;
+
+    private void Start()
+    {
+        min = Vector3.one * minFactor;
+        max = Vector3.one;
+        Debug.Log(min + ", " + max);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Lerp();
+        }
+    }
+
+    public void Lerp()
+    {
+        direction = !direction;
+        Lerp(direction);
+    }
+
+    protected override void OnLerp(float t)
+    {
+        transform.localScale = Vector3.Lerp(min, max, t);
+    }
+
+    protected override void OnLerpFinished(float t)
+    {
+        Debug.Log(t);
+    }
+}
---
+++ Assets/Scripts/SeasonSnowCoverObjectBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonSnowCoverObjectBehaviour : SeasonObjectBehaviour
+{
+    private LerpScaleBehaviour lerpScaleBehaviour;
+
+    public override void Start()
+    {
+        base.Start();
+        lerpScaleBehaviour = GetComponentInChildren<LerpScaleBehaviour>();
+    }
+
+    public override void UpdateRepresentation(Season currentSeason)
+    {
+        base.UpdateRepresentation(currentSeason);
+        Debug.Log(currentSeason);
+        if(currentSeason == Season.WINTER)
+        {
+            lerpScaleBehaviour.Lerp();
+        }
+        else
+        {
+            if(SeasonsManager.Instance.PreviousSeason == Season.WINTER || SeasonsManager.Instance.NextSeason == Season.WINTER)
+            {
+                lerpScaleBehaviour.Lerp();
+            }
+        }
+    }
+}
