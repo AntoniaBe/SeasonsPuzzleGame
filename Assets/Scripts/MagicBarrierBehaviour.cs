bedad3c00a28aa4717ce52abcc0a156543d7ca83
Josef <josef.heitzler@gmx.de>
2019-06-30 15:01:24 +0200

    add magic barrier behaviour

---
+++ Assets/Scripts/MagicBarrierBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBarrierBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player = null;
+
+    [SerializeField]
+    private float minDist;
+
+    private Renderer rend;
+    private Color mainColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        mainColor = rend.material.color;
+    }
+
+    private void Update()
+    {
+        mainColor.a = CustomClamp(
+            minDist / Vector2.Distance(new Vector2(player.position.x, player.position.z), 
+                new Vector2(transform.position.x, transform.position.z)), 
+            0.33f,
+            1f
+        );
+        rend.material.color = mainColor;
+    }
+
+    private float CustomClamp(float f, float min, float max){
+        f = f > max ? max : f;
+        f = f < min ? 0 : f;
+        return f;
+    }
+}
