598d505b23cbb23ba867cfc5a71075cae276e8f0
Antonia Berger <AntoniaBerger@gmx.net>
2019-07-05 10:21:57 +0200

    added red flower animation

---
+++ Assets/FlowerAnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerAnimationScript : MonoBehaviour
+{
+
+    public bool flowerIdle;
+    public bool flowerGrow;
+    public bool flowerBloom;
+    public bool flowerGrowBack;
+
+    public GameObject redFlower;
+    public Animator redFlowerAnimator;
+
+    public GameObject purpleFlower;
+   // public Animator purplelowerAnimator;
+
+
+    void Update()
+    {
+        if (flowerGrow)
+        {
+            redFlowerAnimator.SetBool("FlowersGrow", true);
+            redFlowerAnimator.SetBool("FlowersIdle", false);
+        }
+        else if (flowerBloom)
+        {
+            redFlowerAnimator.SetBool("FlowersBloom", true);
+        }
+        else if (flowerGrowBack)
+        {
+            redFlowerAnimator.SetBool("FlowersGrowBack", true);
+        }
+        else if (flowerIdle)
+        {
+            redFlowerAnimator.SetBool("FlowersIdle", true);
+            redFlowerAnimator.SetBool("FlowersGrow", false);
+            redFlowerAnimator.SetBool("FlowersBloom", false);
+            redFlowerAnimator.SetBool("FlowersGrowBack", false);
+        }
+    }
+}
