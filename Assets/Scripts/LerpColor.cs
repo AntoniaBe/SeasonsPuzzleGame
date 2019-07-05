9557d3090bebd1fa6466207e3d67d048b5471dcc
vncnzd <vincenz.dziedzina@gmail.com>
2019-07-05 17:19:34 +0200

    sunflower colors

---
+++ Assets/Scripts/LerpColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpColor : LerpProperty
+{
+    private Renderer rend;
+    private Color target;
+    private Color start;
+
+    private void Start(){
+        rend = GetComponent<Renderer>();
+    }
+    public void StartLerp(Color target)
+    {
+        this.target = target;
+        start = rend.material.color;
+        Lerp(false);
+    }
+
+    protected override void OnLerp(float t)
+    {
+        var color = Color.Lerp(start, target, t);
+        rend.material.color = color;
+    }
+
+    protected override void OnLerpFinished(float t)
+    {
+        var color = Color.Lerp(start, target, t);
+        rend.material.color = color;
+    }
+}
--- Assets/Scripts/SunflowerSeasonBehaviour.cs
+++ Assets/Scripts/SunflowerSeasonBehaviour.cs
@@ -7,26 +7,68 @@ public class SunflowerSeasonBehaviour : SeasonObjectBehaviour
     [SerializeField]
     private SunflowerAnimationScript animationScript;
 
+    [SerializeField]
+    private Color[] blossomColors;
+
+    [SerializeField]
+    private LerpColor[] blossoms;
+
     public override void UpdateRepresentation(Season currentSeason){
         base.UpdateRepresentation(currentSeason);
         var lastSeason = SeasonsManager.Instance.lastActivatedSeason;
         Debug.Log(currentSeason + ", " + lastSeason);
         switch(currentSeason){
             case Season.SPRING:
-                animationScript.sunflowerAnim.Play(lastSeason == Season.WINTER ? "Grow" : "Bloom_Backwards");
-                break;
-            case Season.SUMMER:
-                animationScript.sunflowerAnim.Play(lastSeason == Season.SPRING ? "Bloom" : "Wilt_Backwards");
+                if(lastSeason == Season.WINTER){
+                    animationScript.sunflowerAnim.Play("Grow");
+                    LerpColor(blossomColors[0]);
+                }else{
+                    animationScript.sunflowerAnim.Play("Bloom_Backwards");
+                    LerpColor(blossomColors[0]);
+                }
                 break;
 
-            case Season.AUTUMN:
-                animationScript.sunflowerAnim.Play(lastSeason == Season.SUMMER ? "Wilt" : "Falling_Backwards");
+            case Season.SUMMER:
+            
+                if(lastSeason == Season.SPRING){
+                    animationScript.sunflowerAnim.Play("Bloom");
+                    LerpColor(blossomColors[1]);
+                }else{
+                    animationScript.sunflowerAnim.Play("Wilt_Backwards");
+                    LerpColor(blossomColors[1]);
+                }
+                break;
+
+            case Season.AUTUMN:           
+                if(lastSeason == Season.SUMMER){
+                    animationScript.sunflowerAnim.Play("Wilt");
+                    LerpColor(blossomColors[2]);
+                }else{
+                    animationScript.sunflowerAnim.Play("Falling_Backwards");
+                    LerpColor(blossomColors[2]);
+                }
                 break;
 
             case Season.WINTER:
-                animationScript.sunflowerAnim.Play(lastSeason == Season.AUTUMN ? "Falling" : "Grow_Backwards");
+                if(lastSeason == Season.AUTUMN){
+                    animationScript.sunflowerAnim.Play("Falling");
+                    LerpColor(blossomColors[2]);
+                }else{
+                    animationScript.sunflowerAnim.Play("Grow_Backwards");
+                    LerpColor(blossomColors[2]);
+                }
                 break;
         }
+    }
+
+    private void LerpColor(Color target){
+        foreach(var blossom in blossoms){
+            blossom.StartLerp(target);
+        }
+    }
+
+    private void SeasonSwitch(){
+        
     }
 
     private void Grow(){
