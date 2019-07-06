394f37def39f39277c5f987f87f40faf0e6f8dec
Antonia Berger <AntoniaBerger@gmx.net>
2019-07-06 21:51:02 +0200

    added chest animation

---
+++ Assets/ChestAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnimation : MonoBehaviour
+{
+
+    public bool chestIdle = true;
+    public bool chestOpen = false;
+    public bool chestClose = false;
+    public Animator chestAnimator;
+
+
+
+    void Update()
+    {
+        if (chestOpen)
+        {
+            chestAnimator.Play("Open");
+        } 
+        else if (chestClose)
+        {
+            chestAnimator.Play("Close");
+        }
+        else if (chestIdle)
+        {
+            chestAnimator.Play("Idle");
+        }
+    }
+}
