c157d15dc3840bbfc53183a1feb3fe81126158c9
Antonia Berger <AntoniaBerger@gmx.net>
2019-07-06 22:51:37 +0200

    fixed wheat animation

--- Assets/Assets_Animations/Scripts/WheatAnimationScript.cs
+++ Assets/Assets_Animations/Scripts/WheatAnimationScript.cs
@@ -8,6 +8,7 @@ public class WheatAnimationScript : MonoBehaviour
     public bool wheatGrowLittle;
     public bool wheatGrowFull;
     public bool wheatGrowBackLittle;
+    public bool wheatGrowBackFull;
     public bool wheatIdle;
     public Animator wheatAnimator;
 
@@ -15,26 +16,26 @@ public class WheatAnimationScript : MonoBehaviour
     {
         if (wheatGrowLittle)
         {
-            wheatAnimator.SetBool("WheatGrowLittle", true);
-            wheatAnimator.SetBool("WheatIdle", false);
+            wheatAnimator.Play("WheatGrowLittle");
         }
 
         else if(wheatGrowFull)
         {
-            wheatAnimator.SetBool("WheatGrowFull", true);
+            wheatAnimator.Play("WheatGrowFull");
         }
         else if (wheatGrowBackLittle)
         {
-            wheatAnimator.SetBool("WheatGrowBackLittle", true);
+            wheatAnimator.Play("WheatGrowBackLittle");
+
+        }
+        else if (wheatGrowBackFull)
+        {
+            wheatAnimator.Play("WheatGrowFullBack");
 
         }
         else if (wheatIdle)
         {
-            wheatAnimator.SetBool("WheatIdle", true);
-            wheatAnimator.SetBool("WheatGrowLittle", false);
-            wheatAnimator.SetBool("WheatGrowFull", false);
-            wheatAnimator.SetBool("WheatGrowBackLittle", false);
-
+            wheatAnimator.Play("WheatIdle");
         }
     }
 }
