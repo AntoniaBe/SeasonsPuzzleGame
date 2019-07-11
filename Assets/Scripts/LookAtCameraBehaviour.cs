6cf5289a3734e68e1650229ec7bcd2fc87ab63d6
Josef <josef.heitzler@gmx.de>
2019-07-11 13:02:25 +0200

    update season stones

---
+++ Assets/Scripts/LookAtCameraBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCameraBehaviour : MonoBehaviour
+{
+    void Update()
+    {
+        if (Camera.main == null)
+            return;
+
+        transform.LookAt(Camera.main.transform);
+    }
+}
