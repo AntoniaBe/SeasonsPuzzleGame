1920129225e896ce90114e830682410702b6f5cc
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2019-07-14 12:45:53 +0200

    Add closed chest sound feature

--- Assets/Scripts/BoxBehavior.cs
+++ Assets/Scripts/BoxBehavior.cs
@@ -1,28 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class BoxBehavior : MonoBehaviour
 {
-    [SerializeField]
     private bool isOpened;
+    public AudioSource closedChestAudioSource;
 
     private void Start(){
-
-    }
-
-    private void Update(){
-        
+        GameObject lid = transform.GetChild(0).gameObject;
+        if (lid.GetComponent<VRTK_InteractableObject>())
+        {
+            lid.GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += PlayClosedChestSound;
+        }
+        else
+        {
+            Debug.LogError("lid of chest has no VRTK_InteractableObject component");
+        }
     }
 
     void OnCollisionEnter (Collision col)
     {
-        if(col.gameObject.tag == "Key")
+        if(col.gameObject.tag == "Key" && !isOpened)
         {
             gameObject.transform.Find("SM_Prop_Chest_Wood_Lid").GetComponent<Animator>().Play("Open");
             StateManager stateManager = StateManager.Instance;
             stateManager.ChestOpen();
             isOpened = true;
+        }
+    }
+
+    private void PlayClosedChestSound(object sender, InteractableObjectEventArgs e)
+    {
+        if (!isOpened)
+        {
+            closedChestAudioSource.Play();
         }
     }
 }
